2b466f0ff2278f65560ecde7064255b63daf4689
Valeri Dobrev <valeri.dobrev10@gmail.com>
2020-12-10 00:11:31 +0200

    Added file choosing, getters and changed the font size

--- clothesarchive/gui/customSettings/CustomFonts.java
+++ clothesarchive/gui/customSettings/CustomFonts.java
@@ -5,13 +5,13 @@ import java.awt.*;
 public class CustomFonts {
 
     public static Font HeadingFont(){
-        return new Font("Ariel", Font.PLAIN,24);
+        return new Font("Ariel", Font.PLAIN,28);
     }
 
     public static Font HintFont(){
-        return new Font("Ariel", Font.ITALIC, 10);
+        return new Font("Ariel", Font.ITALIC, 14);
     }
     public static Font TextBoxTextFont(){
-        return new Font("Ariel", Font.PLAIN, 14);
+        return new Font("Ariel", Font.PLAIN, 18);
     }
 }
--- clothesarchive/gui/panels/mainContent/AddMenuContent.java
+++ clothesarchive/gui/panels/mainContent/AddMenuContent.java
@@ -1,28 +1,41 @@
 package clothesarchive.gui.panels.mainContent;
 
+import clothesarchive.MainScreen;
+import clothesarchive.gui.customSettings.CustomFonts;
 import clothesarchive.gui.customSettings.HintTextField;
 import clothesarchive.gui.customSettings.FormattedFieldListener;
+import customFilters.CustomPictureFilter;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.text.NumberFormat;
 import java.util.Currency;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-public class AddMenuContent extends JPanel {
+public class AddMenuContent extends JPanel implements ActionListener {
     HintTextField name;
     HintTextField description;
     HintTextField company;
     JFormattedTextField price;
+    byte[] file;
+    JFileChooser fileChooser;
+    JTextField filePath;
 
     public AddMenuContent(){
         this.setPreferredSize(new Dimension(100,100));
-
-        this.setLayout(new GridLayout(5,0));
+        this.setLayout(new GridLayout(7,0));
 
         this.add(this.createFields(1)); //Creating name field
         this.add(this.createFields(2)); //Creating description field
         this.add(this.createFields(3)); //Creating company name field
         this.add(this.createPriceField());  //Creating price field
+        this.add(this.createFileUpload());  //Creating file upload button
 
 
         this.setVisible(true);
@@ -51,7 +64,7 @@ public class AddMenuContent extends JPanel {
             this.company=textField;
         }
 
-        return setupContentLayout(textField);
+        return setupContentLayout(textField, null);
     }
     /**
      * Method for creating the price field
@@ -65,34 +78,112 @@ public class AddMenuContent extends JPanel {
         this.price = new JFormattedTextField(nf);
         this.price.setValue(0);
         this.price.setColumns(10);
+        this.price.setFont(CustomFonts.TextBoxTextFont());
 
         this.price.setPreferredSize(new Dimension(100,50));
         this.price.addFocusListener(new FormattedFieldListener(this.price));
 
-        return this.setupContentLayout(this.price);
+        return this.setupContentLayout(this.price, null);
     }
 
     /**
      * Method for creating 2 containers so that the fields can be aligned easier
      * @param jTextField :
      *            Field to be added to the 2 containers
+     * @param button:
+     *              Button to be added
      * @return JPanel
      *
      */
-    private JPanel setupContentLayout(JTextField jTextField){
+    private JPanel setupContentLayout(JTextField jTextField,JButton button){
         JPanel borderContainer = new JPanel(new BorderLayout()); //Creating border panel that will contain fieldContainer
-
         JPanel flowContainer = new JPanel(new FlowLayout(FlowLayout.CENTER)); //Creating the field container
 
         flowContainer.add(jTextField); //Adding the text field to the flow container
+        if(button!=null) {
+            flowContainer.add(button); //Adding button if provided
+        }
         borderContainer.add(flowContainer, BorderLayout.SOUTH); //Adding the the flow container in the bottom of the border container
 
         return borderContainer;
     }
-    private JPanel createFileUpload(){
 
-        return null;
+    /**
+     * Method for creating the upload files field an button
+     *
+     * @return JPanel
+     */
+    private JPanel createFileUpload(){
+        JButton selectButton = new JButton("Избор на файл...");
+        this.filePath = new JTextField();
+
+        this.filePath.setPreferredSize(new Dimension(400,50));
+        this.filePath.setAutoscrolls(true);
+
+
+        selectButton.setPreferredSize(new Dimension(100,45));
+
+        selectButton.addActionListener(this);
+
+        this.fileChooser=new JFileChooser();
+        this.fileChooser.setFileFilter(new CustomPictureFilter());
+        this.fileChooser.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
+
+        return this.setupContentLayout(filePath, selectButton);
     }
 
+    /**
+     * Method handling the chooseFile button
+     */
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        this.file = null; //setting the file bytes to null
+        int returnVal = fileChooser.showOpenDialog(this); //Setting the opening dialog
+        if (returnVal == JFileChooser.APPROVE_OPTION) {
+            File file = fileChooser.getSelectedFile();  //Getting the selected file
+            this.filePath.setText(file.getAbsolutePath());  //Setting the file path to the text field
 
+            try {
+                this.file = Files.readAllBytes(file.toPath()); //Converting the file to bytes[]
+            } catch (IOException ex) {
+                Logger.getLogger(MainScreen.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        } else {
+            System.out.println("File access cancelled by user.");
+        }
+    }
+    /**
+     * Method for clearing all the fields
+     */
+    public void clearAllFields(){
+        this.filePath.setText("");
+        this.price.setValue(0);
+        this.company.setText("");
+        this.description.setText("");
+        this.name.setText("");
+        this.file=null;
+    }
+
+    /**
+     * Getters for the fields that are needed for the backend
+     */
+    public HintTextField getNameField() {
+        return name;
+    }
+
+    public HintTextField getDescription() {
+        return description;
+    }
+
+    public HintTextField getCompany() {
+        return company;
+    }
+
+    public JFormattedTextField getPrice() {
+        return price;
+    }
+
+    public byte[] getFile() {
+        return file;
+    }
 }
