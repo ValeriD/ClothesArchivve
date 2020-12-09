9dd308530b31c951a6cfa2793b8e9991a1c5198f
Valeri Dobrev <valeri.dobrev10@gmail.com>
2020-12-09 14:09:51 +0200

    Fixed the buttons location, commented the methods and finished the panel

--- clothesarchive/gui/panels/buttons/AddMenuButtons.java
+++ clothesarchive/gui/panels/buttons/AddMenuButtons.java
@@ -1,60 +1,55 @@
 package clothesarchive.gui.panels.buttons;
 
+import clothesarchive.exceptions.CAException;
 import clothesarchive.gui.panels.general.AddMenu;
 
 import javax.swing.*;
 import java.awt.*;
 
 public class AddMenuButtons extends JPanel {
-    JButton addButton;
-    JButton cancelButton;
-    AddMenu addMenu;
+    AddMenu addMenu; //Instance of the AddMenu so that a listener could be connected
+
     public AddMenuButtons(AddMenu addMenu){
         this.addMenu=addMenu;
+        this.setPreferredSize(new Dimension(100,55)); //Setting the size of the panel
 
 
-        this.setLayout(new GridLayout(1,2)); //Setting the bottom layout to Grid so that 2 flow panels can fit
 
-        this.add(createAddButton()); //Adding the left panel
-        this.add(createCancelButton());//Adding the right panel
+        this.setLayout(new GridLayout(0,2)); //Setting the bottom layout to Grid so that 2 flow panels can fit
+
+        this.add(createButton(1)); //Adding the left panel
+        this.add(createButton(0));//Adding the right panel
 
         this.setVisible(true);
 
     }
+    /**
+     * Method for creating aligned buttons
+     * @param flag :
+     *             1 if the button is for adding
+     *             0 if the button is for canceling
+     * @return JPanel
+     */
+    private JPanel createButton(int flag){
+        JButton button= new JButton(); //Creating new button
+        button.setPreferredSize(new Dimension(200,45)); //Setting the size
+        button.addActionListener(this.addMenu); //Adding a listener for the button
 
-    private JPanel createAddButton(){
-        this.addButton = new JButton(); //Creating new button
-        this.addButton.setText("Добави"); //Setting the text for the button
-        this.addButton.setPreferredSize(new Dimension(200,45));//Setting the size of the button
-        this.addButton.addActionListener(this.addMenu);
+        JPanel buttonContainer = new JPanel(); //Creating a panel for the button
 
-        JPanel leftButton = new JPanel();//Creating new panel for my AddButton
-        leftButton.setLayout(new FlowLayout(FlowLayout.LEADING));//Setting the FlowLayout so the button goes right
-        leftButton.add(addButton);//Adding the button in my new panel
-
-        return leftButton;
+        //Checks if which button is wanted to be created
+        if(flag==1){
+            button.setText("Добави");//Setting the text for the button
+            buttonContainer.setLayout(new FlowLayout(FlowLayout.LEADING)); //Creating FlowLayout so that the button is aligned left
+        }else{
+            button.setText("Откажи"); //Setting the text for the button
+            buttonContainer.setLayout(new FlowLayout(FlowLayout.TRAILING)); //Creating FlowLayout so that the button is aligned right
+        }
+        buttonContainer.add(button); //Adding the button to the panel
+        return buttonContainer;
     }
 
-    private JPanel createCancelButton(){
 
-        this.cancelButton = new JButton();  //Creating new button
-        this.cancelButton.setText("Откажи"); //Setting the text for the button
-        this.cancelButton.setPreferredSize(new Dimension(200,45)); //Setting the size of the button
-        this.cancelButton.addActionListener(this.addMenu);
 
-        JPanel rightButton = new JPanel(); //Creating new panel for my CancelButton
-        rightButton.setLayout(new FlowLayout(FlowLayout.TRAILING));//Setting the FlowLayout so the button goes right
-        rightButton.add(this.cancelButton); //Adding the button in my new panel
-
-        return rightButton;
-    }
-
-    public JButton getAddButton(){
-        return this.addButton;
-    }
-
-    public JButton getCancelButton() {
-        return cancelButton;
-    }
 
 }
